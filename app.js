bdfe3899296fe0775d202e936da6f7dc65100743
geremewatalay <geremewataly@gmail.com>
2024-12-13 22:52:18 -0800

    controller database routes commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 5000
+
+
+//db connection
+const dbConnection = require("./db/dbConfig")
+
+
+//user routers middleware file
+const userRoutes = require("./routes/userRoute")
+
+//user routes middleware
+app.use("/api/users",userRoutes)
+
+// questions routes middleware
+
+//answers routes middleware
+async function start() {
+   try {
+    const result = await dbConnection.execute("select 'test' ")
+    await app.listen(port)
+    console.log("database connection established");
+    console.log(`listening on ${port}`)
+       
+} catch (error) {
+    console.log(error.message)
+} 
+}
+start()
+
+
+
+
+
---
+++ controller/userController.js
@@ -0,0 +1,16 @@
+function register(req,res) {
+    res.send("register")
+    
+}
+
+function login(req,res) {
+    res.send("login")
+    
+}
+
+function checkUser(req,res) {
+    res.send("check user")
+    
+}
+
+module.exports = {register, login, checkUser}
